e157ed876dae907c09e8cd4ad3279566b98cb17b
helloaltop <helloaltop@gmail.com>
2022-06-17 20:16:07 +0800

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -2,7 +2,7 @@
 
 RTC_DS1307 rtc;
 
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+String daysOfTheWeek[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 void setup () {
   Serial.begin(115200);
